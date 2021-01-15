a95b44d551395b150bdb1a1cfb6c72053a972c20
Leandro Meireles <lmmeireles11@gmail.com>
2021-01-15 10:54:56 -0300

    primeiro commit

---
+++ src/main/java/com/eventoapp/PetzappApplication.java
@@ -0,0 +1,13 @@
+package com.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetzappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetzappApplication.class, args);
+	}
+
+}
